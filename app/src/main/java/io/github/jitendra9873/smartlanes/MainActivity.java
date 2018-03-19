1949fd5f2e06745c9170b8406523139da37276e1
Cyprien Dcunha <dcunha.cyprien@gmail.com>
2018-03-20 01:26:12 +0530

    Add navigation drawer

--- app/src/main/java/io/github/jitendra9873/smartlanes/MainActivity.java
+++ app/src/main/java/io/github/jitendra9873/smartlanes/MainActivity.java
@@ -2,35 +2,94 @@ package io.github.jitendra9873.smartlanes;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.bluetooth.le.AdvertiseCallback;
-import android.bluetooth.le.AdvertiseSettings;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import org.altbeacon.beacon.Beacon;
-import org.altbeacon.beacon.BeaconParser;
-import org.altbeacon.beacon.BeaconTransmitter;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 
 import java.lang.ref.WeakReference;
-import java.util.Arrays;
+
+import io.github.jitendra9873.smartlanes.fragments.HomeFragment;
 
 import static io.github.jitendra9873.smartlanes.LoginActivity.SP_LOGIN_ID;
 import static io.github.jitendra9873.smartlanes.LoginActivity.SP_LOGIN_LOGGED_IN_STATE;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         final ProgressDialog progressDialog = new ProgressDialog(this);
         new LoginTask(this, progressDialog).execute();
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,
+                toolbar, R.string.nav_bar_open, R.string.nav_bar_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame, new HomeFragment());
+        ft.commit();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        if(drawerLayout.isDrawerOpen(Gravity.START)){
+            drawerLayout.closeDrawer(Gravity.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        displaySelectedScreen(item.getItemId());
+        return false;
+    }
+
+    private void displaySelectedScreen(int itemId) {
+        android.support.v4.app.Fragment fragment = null;
+
+        switch (itemId){
+            case R.id.nav_menu_home:
+                fragment = new HomeFragment();
+                break;
+        }
+
+        if(fragment != null){
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
+        }
+
+        if(drawerLayout.isDrawerOpen(Gravity.START)){
+            drawerLayout.closeDrawer(Gravity.START);
+        }
     }
 
     private static class LoginTask extends AsyncTask<Void, Void, Void> {
---
+++ app/src/main/java/io/github/jitendra9873/smartlanes/fragments/HomeFragment.java
@@ -0,0 +1,22 @@
+package io.github.jitendra9873.smartlanes.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.github.jitendra9873.smartlanes.R;
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
+    }
+
+}
