0c451bce6b67d435edb80548c462474c76eda2b4
jitendra9873 <jitendrakumhar1@gmail.com>
2018-03-19 22:07:24 +0530

    initial commit

---
+++ app/src/main/java/io/github/jitendra9873/smartlanes/MainActivity.java
@@ -0,0 +1,22 @@
+package io.github.jitendra9873.smartlanes;
+
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseSettings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.BeaconTransmitter;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
