67b63c9ae37a5ba9a32ae957c69e426eecca0ad1
Cyprien Dcunha <dcunha.cyprien@gmail.com>
2018-03-19 22:53:53 +0530

    Add login to app

---
+++ app/src/main/java/io/github/jitendra9873/smartlanes/LoginActivity.java
@@ -0,0 +1,205 @@
+package io.github.jitendra9873.smartlanes;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+import java.util.Objects;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // Login Preferences
+    public static final String SP_LOGIN_ID = "LoginPreferences";
+    public static final String SP_LOGIN_LOGGED_IN_STATE = "LoggedInState";
+    public static final String SP_LOGIN_USER_NAME = "Username";
+
+    // Lock to avoid deadlock
+    private static boolean lockLogin = false;
+
+    // UI references.
+    private EditText usernameET;
+    private EditText passwordView;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Set up the login form.
+        usernameET = findViewById(R.id.login_username);
+
+        passwordView = findViewById(R.id.login_password);
+        passwordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == 907 || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button emailSignInButton = findViewById(R.id.login_sign_in);
+        emailSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+        setupProgressDialog();
+    }
+
+    private void setupProgressDialog() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setIndeterminate(true);
+    }
+
+    private void attemptLogin() {
+        if (lockLogin) {
+            return;
+        }
+
+        // Reset errors.
+        usernameET.setError(null);
+        passwordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = usernameET.getText().toString();
+        String password = passwordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(password) || !isValidPassword(password)) {
+            passwordView.setError(getString(R.string.error_invalid_password));
+            focusView = passwordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            usernameET.setError(getString(R.string.error_field_required));
+            focusView = usernameET;
+            cancel = true;
+        } else if (!isValidUsername(email)) {
+            usernameET.setError(getString(R.string.error_invalid_email));
+            focusView = usernameET;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            progressDialog.setMessage(getString(R.string.login_authenticating));
+            progressDialog.show();
+            signIn(email, password);
+        }
+    }
+
+    static boolean isValidUsername(String username) {
+        return username.matches(".+@.+\\..+");
+    }
+
+    static boolean isValidPassword(String password) {
+        return password.length() >= 8;
+    }
+
+    void signIn(final String username, final String password) {
+        new signInTask(this, username, password, usernameET, passwordView, progressDialog).execute();
+    }
+
+    private static class signInTask extends AsyncTask<Void, Void, Integer> {
+
+        WeakReference<Context> wrContext;
+        WeakReference<Activity> wrActivity;
+        WeakReference<EditText> wrUsernameET, wrPassET;
+        String username, password;
+        ProgressDialog progressDialog;
+
+        //String login pass (Dummy Data)
+        String[][] loginData = {
+                {"guest@guest.com", "guest123"}
+        };
+
+        signInTask(Activity activity, final String username, final String password,
+                   EditText usernameET, EditText passET, ProgressDialog progressDialog){
+            this.wrActivity = new WeakReference<>(activity);
+            this.wrContext = new WeakReference<>(activity.getApplicationContext());
+            this.wrUsernameET = new WeakReference<>(usernameET);
+            this.wrPassET = new WeakReference<>(passET);
+            this.username = username;
+            this.password = password;
+            this.progressDialog = progressDialog;
+        }
+
+        @Override
+        protected void onPreExecute(){
+            lockLogin = true;
+        }
+
+        @Override
+        protected Integer doInBackground(Void... params) {
+            int idx = -1;
+            for(int i=0; i<loginData.length; i++){
+                if(Objects.equals(loginData[i][0], username)){
+                    if(Objects.equals(loginData[i][1], password)){
+                        idx = i;
+                        break;
+                    }
+                }
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return idx;
+
+        }
+
+        @Override
+        protected void onPostExecute(Integer response){
+            if(response != -1){
+                SharedPreferences prefs = wrContext.get().getSharedPreferences(SP_LOGIN_ID, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+
+                editor.putBoolean(SP_LOGIN_LOGGED_IN_STATE, true);
+                editor.putString(SP_LOGIN_USER_NAME, username);
+
+                editor.apply();
+
+                wrActivity.get().finish();
+                wrContext.get().startActivity(new Intent(wrContext.get(), MainActivity.class));
+            }
+            else{
+                wrUsernameET.get().setError(wrContext.get().getString(R.string.error_incorrect_password));
+                wrPassET.get().setError(wrContext.get().getString(R.string.error_incorrect_password));
+                wrPassET.get().requestFocus();
+            }
+            lockLogin = false;
+            progressDialog.dismiss();
+        }
+    }
+
+}
--- app/src/main/java/io/github/jitendra9873/smartlanes/MainActivity.java
+++ app/src/main/java/io/github/jitendra9873/smartlanes/MainActivity.java
@@ -1,7 +1,13 @@
 package io.github.jitendra9873.smartlanes;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.bluetooth.le.AdvertiseCallback;
 import android.bluetooth.le.AdvertiseSettings;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,7 +16,11 @@ import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.BeaconTransmitter;
 
+import java.lang.ref.WeakReference;
 import java.util.Arrays;
+
+import static io.github.jitendra9873.smartlanes.LoginActivity.SP_LOGIN_ID;
+import static io.github.jitendra9873.smartlanes.LoginActivity.SP_LOGIN_LOGGED_IN_STATE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +28,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        new LoginTask(this, progressDialog).execute();
+    }
+
+    private static class LoginTask extends AsyncTask<Void, Void, Void> {
+
+        WeakReference<Activity> wrActivity;
+        WeakReference<Context> wrContext;
+        ProgressDialog progressDialog;
+
+        LoginTask(Activity activity, ProgressDialog pd){
+            wrActivity = new WeakReference<>(activity);
+            wrContext = new WeakReference<>(activity.getApplicationContext());
+            this.progressDialog = pd;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog.setIndeterminate(true);
+            progressDialog.setMessage(wrContext.get().getString(R.string.login_logging_in));
+            progressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            SharedPreferences prefs = wrContext.get().getSharedPreferences(SP_LOGIN_ID, Context.MODE_PRIVATE);
+            boolean isLoggedIn = prefs.getBoolean(SP_LOGIN_LOGGED_IN_STATE, false);
+
+            if(!isLoggedIn){
+                wrActivity.get().finish();
+                wrActivity.get().startActivity(new Intent(wrActivity.get(), LoginActivity.class));
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPreExecute();
+            progressDialog.dismiss();
+        }
+
     }
 }
