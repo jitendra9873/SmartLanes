1e69c6b666770c80ecc1033090a7ee3be41bafee
Cyprien Dcunha <dcunha.cyprien@gmail.com>
2018-03-20 12:58:36 +0530

    add editprofile and add vehicle

--- app/src/main/java/io/github/jitendra9873/smartlanes/EditProfileActivity.java
+++ app/src/main/java/io/github/jitendra9873/smartlanes/EditProfileActivity.java
@@ -1,14 +1,80 @@
 package io.github.jitendra9873.smartlanes;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class EditProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_editprofile);
 
+        Toolbar toolbar = findViewById(R.id.ep_toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        EditText usernameET = findViewById(R.id.edit_profile_username);
+        EditText aadHarET = findViewById(R.id.edit_profile_aadhar);
+        EditText licenceET = findViewById(R.id.edit_profile_licence);
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Verifying");
+
+        final Activity activity = this;
+
+        FloatingActionButton fab = findViewById(R.id.et_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("StaticFieldLeak")
+            @Override
+            public void onClick(View v) {
+                new AsyncTask<Void, Void, Void>(){
+
+                    @Override
+                    protected void onPreExecute(){
+                        progressDialog.show();
+                    }
+
+                    @Override
+                    protected Void doInBackground(Void... voids) {
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Void aVoid){
+                        progressDialog.dismiss();
+                        activity.finish();
+                    }
+                }.execute();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/io/github/jitendra9873/smartlanes/fragments/AccountFragment.java
+++ app/src/main/java/io/github/jitendra9873/smartlanes/fragments/AccountFragment.java
@@ -1,6 +1,12 @@
 package io.github.jitendra9873.smartlanes.fragments;
 
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,17 +18,21 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
+import io.github.jitendra9873.smartlanes.EditProfileActivity;
 import io.github.jitendra9873.smartlanes.R;
 
 public class AccountFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_account, container, false);
+        final View view = inflater.inflate(R.layout.fragment_account, container, false);
 
         setHasOptionsMenu(true);
 
@@ -38,18 +48,85 @@ public class AccountFragment extends Fragment {
 
         final RadioButton button1 = view.findViewById(R.id.vt_rb1);
         final RadioButton button2 = view.findViewById(R.id.vt_rb2);
+        final RadioButton button3 = view.findViewById(R.id.vt_rb3);
 
         button1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked) button2.setChecked(false);
+                if(isChecked) {
+                    button2.setChecked(false);
+                    button3.setChecked(false);
+                }
             }
         });
 
         button2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked) button1.setChecked(false);
+                if(isChecked) {
+                    button1.setChecked(false);
+                    button3.setChecked(false);
+                }
+            }
+        });
+
+        button3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    button1.setChecked(false);
+                    button2.setChecked(false);
+                }
+            }
+        });
+
+        Button addButton = view.findViewById(R.id.add_vehicle_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Add Vehicle");
+                builder.setView(R.layout.dialog_addvehicle);
+                builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @SuppressLint("StaticFieldLeak")
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        new AsyncTask<Void, Void, Void>(){
+
+                            ProgressDialog progressDialog = new ProgressDialog(getContext());
+
+                            @Override
+                            protected void onPreExecute(){
+                                progressDialog.setMessage("Verifying");
+                                progressDialog.show();
+                            }
+
+                            @Override
+                            protected Void doInBackground(Void... voids) {
+                                try {
+                                    Thread.sleep(2000);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                return null;
+                            }
+
+                            @Override
+                            protected void onPostExecute(Void aVoid){
+                                progressDialog.dismiss();
+                                View newVehRow = view.findViewById(R.id.table_hidden_row);
+                                newVehRow.setVisibility(View.VISIBLE);
+                            }
+                        }.execute();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.create().show();
             }
         });
 
@@ -67,6 +144,8 @@ public class AccountFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_account_edit:
+                Intent intent = new Intent(getContext(), EditProfileActivity.class);
+                startActivity(intent);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
